473cc0f0f9ef106854e6cc07e205ffc14ecb77ec
zhutmost <zhutmost@outlook.com>
2021-09-24 17:28:22 +0800

    Change Bool to Bool()

--- src/main/scala/mylib/MyTopLevel.scala
+++ src/main/scala/mylib/MyTopLevel.scala
@@ -7,9 +7,9 @@ import spinal.lib._
 //Hardware definition
 class MyTopLevel extends Component {
   val io = new Bundle {
-    val cond0 = in  Bool
-    val cond1 = in  Bool
-    val flag  = out Bool
+    val cond0 = in  Bool()
+    val cond1 = in  Bool()
+    val flag  = out Bool()
     val state = out UInt(8 bits)
   }
   val counter = Reg(UInt(8 bits)) init(0)
