bfaa079cd3434cb301e53f69b8644db26e9dc5d7
zhutmost <zhutmost@outlook.com>
2022-05-03 02:53:35 +0800

    Add Scalafmt support

--- mylib/src/mylib/MyTopLevel.scala
+++ mylib/src/mylib/MyTopLevel.scala
@@ -3,18 +3,17 @@ package mylib
 import spinal.core._
 import spinal.lib._
 
-
-//Hardware definition
+/** Hardware definition */
 class MyTopLevel extends Component {
   val io = new Bundle {
-    val cond0 = in  Bool()
-    val cond1 = in  Bool()
-    val flag  = out Bool()
-    val state = out UInt(8 bits)
+    val cond0 = in Bool ()
+    val cond1 = in Bool ()
+    val flag  = out Bool ()
+    val state = out UInt (8 bits)
   }
-  val counter = Reg(UInt(8 bits)) init(0)
+  val counter = Reg(UInt(8 bits)) init (0)
 
-  when(io.cond0){
+  when(io.cond0) {
     counter := counter + 1
   }
 
@@ -22,21 +21,21 @@ class MyTopLevel extends Component {
   io.flag  := (counter === 0) | io.cond1
 }
 
-//Generate the MyTopLevel's Verilog
+/** Generate the MyTopLevel's Verilog */
 object MyTopLevelVerilog extends App {
   SpinalVerilog(new MyTopLevel)
 }
 
-//Generate the MyTopLevel's VHDL
+/** Generate the MyTopLevel's VHDL */
 object MyTopLevelVhdl extends App {
   SpinalVhdl(new MyTopLevel)
 }
 
+/** Define a custom SpinalHDL configuration with synchronous reset instead of the default asynchronous one. This configuration can be reused everywhere */
+object MySpinalConfig
+    extends SpinalConfig(defaultConfigForClockDomains = ClockDomainConfig(resetKind = SYNC))
 
-//Define a custom SpinalHDL configuration with synchronous reset instead of the default asynchronous one. This configuration can be resued everywhere
-object MySpinalConfig extends SpinalConfig(defaultConfigForClockDomains = ClockDomainConfig(resetKind = SYNC))
-
-//Generate the MyTopLevel's Verilog using the above custom configuration.
+/** Generate the MyTopLevel's Verilog using the above custom configuration. */
 object MyTopLevelVerilogWithCustomConfig extends App {
   MySpinalConfig.generateVerilog(new MyTopLevel)
 }
